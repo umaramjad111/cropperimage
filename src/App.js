7004cc7ea5c058d2e73e5c8f72c22ac6a9abca8e
umaramjad <umaramjad489@gmail.com>
2023-08-30 16:36:27 +0500

    initlacommit2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import CropperImage from "./components/cropper/cropper";
 import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CropperImage />
     </div>
   );
 }
